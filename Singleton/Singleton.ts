d2c830f634cb7ad1c0b99dea680f4a02f3409e4c
bohdanbirdie <voja4ok@yahoo.com>
2018-02-03 18:01:02 +0200

    fixed Singleton, recovered Builder

--- Singleton/Singleton.ts
+++ Singleton/Singleton.ts
@@ -2,8 +2,8 @@ class Database {
     private static instance: Database;
     private queryCalls: number = 0;
 
-    static getInstace(){
-        if (this.instance){
+    static getInstace() {
+        if (this.instance) {
             console.log('returning already created instance');
             return this.instance;
         } else {
@@ -13,11 +13,11 @@ class Database {
         }
     }
 
-    constructor(){
+    private constructor() {
         console.log('Connecting to the database...')
     }
 
-    query(params: string): string{
+    query(params: string): string {
         return `query was called ${this.queryCalls} times on this instance, params -> ${params}`;
     }
 }
@@ -25,19 +25,18 @@ class Database {
 class Application {
     database: Database;
 
-    constructor(){
+    constructor() {
         this.database = Database.getInstace();
 
         this.database.query('SELECT * from *;')
         this.database.query('SELECT null from *;')
     }
 
-    trySingleton(): void{
+    trySingleton(): void {
         this.database = Database.getInstace();
         this.database.query('SELECT null from null;')
     }
 }
-
 
 const app: Application = new Application();
 app.trySingleton();
